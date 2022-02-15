cc89eeae273f59fc47d69b6376160862d6ca14f2
TT38294 <tt.cdpbl-mylv0823@i.softbank.jp>
2022-02-15 18:55:39 +0900

    テストコード修正2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,24 +1,22 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
-    email                 {"test@com"}
-    encrypted_password    {"test1111"}
-    password_confirmation {"test1111"}
-    family_name           {"山田"}
-    first_name            {"太郎"}
-    family_name_kana      {"ヤマダ"}
-    first_name_kana       {"タロウ"}
-    birth_day             {"2000-01-01"}
+    nickname              { 'test' }
+    email                 { 'test@com' }
+    encrypted_password    { 'test1111' }
+    password_confirmation { 'test1111' }
+    family_name           { '山田' }
+    first_name            { '太郎' }
+    family_name_kana      { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth_day             { '2000-01-01' }
   end
 end
 
 class User < ApplicationRecord
-
   validates :nickname,               presence: true
   validates :encrypted_password,     presence: true, length: { minimum: 6 }
-  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-
+  validates :family_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
