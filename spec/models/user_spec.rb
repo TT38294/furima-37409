412f683cc754952378522ca394773152a34c2c5f
TT38294 <tt.cdpbl-mylv0823@i.softbank.jp>
2022-02-16 14:47:16 +0900

    コード修正4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -91,10 +91,22 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Family name can't be blank")
       end
 
+      it "family_nameが半角の場合は登録できないこと" do
+        @user.family_name = 'ｱｲｳｴｵ'
+        @user.valid?
+        expect(user.errors[:family_name]).to include("Family name is invalid")
+      end
+
       it "first_nameがない場合は登録できない" do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+
+      it "first_nameが半角の場合は登録できないこと" do
+        @user.family_name = 'ｱｲｳｴｵ'
+        @user.valid?
+        expect(user.errors[:family_name]).to include("First name is invalid")
       end
 
       it "family_name_kanaがない場合は登録できない" do
@@ -103,10 +115,22 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Family name kana can't be blank")
       end
 
+      it "family_name_kanaにカタカナ以外の文字が入力されている場合は登録できない" do
+        @user.family_name_kana = 'あいうえお'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana is invalid")
+      end
+
       it "first_name_kanaがない場合は登録できない" do
         @user.first_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+
+      it "first_name_kanaにカタカナ以外の文字が入力されている場合は登録できない" do
+        @user.first_name_kana = 'あいうえお'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
       end
 
       it "birth_dayがない場合は登録できない" do
