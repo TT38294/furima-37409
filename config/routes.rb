0a062d9f8d4615031a9e4440d265c9be6d71b00c
TT38294 <tt.cdpbl-mylv0823@i.softbank.jp>
2022-02-16 16:50:35 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index" 
+<<<<<<< Updated upstream
 #  resources :items, only: [:new, :create]
+=======
+  resources :items, only: [:new, :create]
+>>>>>>> Stashed changes
 end
