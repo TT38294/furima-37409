4924a82c1398e2c42790ad6c8bb4d14d90eb6c3c
TT38294 <tt.cdpbl-mylv0823@i.softbank.jp>
2022-02-22 19:40:13 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,9 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
+    if @item.user_id == current_user.id
+      @item.destroy
       redirect_to root_path
     end
   end
